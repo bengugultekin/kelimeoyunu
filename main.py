844a6b3d73606f97f19f39be9694c189d71c20bf
Bengunur Gultekin <bengunurgultekin54@gmail.com>
2018-12-04 18:58:26 +0300

    some changes were made

--- main.py
+++ main.py
@@ -1,8 +1,48 @@
 import pandas as pd
 from random import randint
 
+
+
 data = pd.read_csv('sorular.csv', sep=';')
 puan = 0
+cevap = "_"
+
+
+def liste_olustur(liste, sınır):
+    sınır = list(sınır)
+    liste = list(liste)
+    for i in range(0, len(sınır) - 1):
+        liste.append("_")
+    return liste
+
+
+def listetemizle(liste):
+    for i in range(0, len(liste)):
+        liste[i] = "_"
+
+
+def bul(girdi, hedef):
+    hedef = list(hedef)
+    for i in range(0, len(hedef)):
+        if (hedef[i] == girdi):
+            return True
+
+
+"""
+def kontrolet(hedef):
+    hedef = list(hedef)
+    for i in range (0,hedef):
+        if hedef[i]=="_":
+           return False
+    return True 
+"""
+
+
+def degistir(girdi, hedef):
+    hedef = list(hedef)
+    for i in range(0, len(hedef)):
+        if hedef[i] == girdi:
+            hedef[i] = girdi
 
 
 def sayi_uret(random):
@@ -11,6 +51,8 @@ def sayi_uret(random):
         new_random = randint(0, 9)
         ayni = random == new_random
     return new_random
+
+    kontrol = 0
 
 
 for i in range(1, 8):
@@ -22,17 +64,46 @@ for i in range(1, 8):
         kelime = sorular.iloc[random]['kelime']
         harf = sorular.iloc[random]['harf']
         print(soru)
-        print(kelime)
+        # print(str(kelime))
 
         for i in range(harf):
             print(' _ ', end='')
 
-        print(kelime)
-        tahmin = input()
+        # print(kelime)
 
-        if tahmin == kelime:
-            print("tebrikler")
-            puan = puan + harf * 100
-            print(puan)
+        kelime = list(kelime)
+        # - işeretlerini bir listeye koyup bilinene kelimeleri eşelştigi şekilde - sembolunun uzerine override edicek
+        # - işaretlerini override etmesi için ayrı bir fonksyon cıkar
+        cevap = liste_olustur(cevap, kelime)
+        kontrol = 0
+        while kontrol < 1:
+            tahmin = input()
+            sonuc = bul(tahmin, kelime)
+            if sonuc == True:
+                for k in range(0, len(kelime)):
+                    if kelime[k] == tahmin:
+                        cevap[k] = tahmin
+                print("tahmininiz kelimede var \n")
+                puan = puan + 100
+            else:
+                print("Yanlış girdiniz")
+                if cevap == kelime:
+                    kontrol = kontrol + 1
+                    listetemizle(cevap)
+
+                """
+                if len(kelime) == len(cevap): # bu olmazsa len lerinin eşitligi uzerinden hareket et zaten sadece dogruları atıyosun cevapların içine
+                    kontrol = kontrol +1
+                    print("Kelimenin tamamını buldunuz %d \n",puan)
+                """
+            print(soru)
+            # print(str(kelime))
+            print(cevap)
+            continue
+
+        # if tahmin == kelime:
+        #  print("tebrikler")
+        #  puan = puan + harf * 100
+        #   print(puan)
 
         print(tahmin)
